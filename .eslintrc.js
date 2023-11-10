bad157564e8e96afc153c00788acb8b0465d1650
Andrey Babkin <amibabkin@gmail.com>
2023-11-10 13:32:10 +0700

    initialize repository

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@html-eslint', 'prettier'],
+  overrides: [
+    {
+      files: ['*.html'],
+      parser: '@html-eslint/parser',
+      extends: ['plugin:@html-eslint/recommended'],
+    },
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@html-eslint/indent': ['error', 2],
+    '@html-eslint/require-closing-tags': ['error', { selfClosing: 'always' }],
+    '@html-eslint/no-extra-spacing-attrs': [
+      'error',
+      { enforceBeforeSelfClose: true },
+    ],
+  },
+};
